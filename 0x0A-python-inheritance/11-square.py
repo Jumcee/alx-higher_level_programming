1857b88a576c38c998af6f0c0e30a646c2c4d5d9
Jumcee <futhmah456@gmail.com>
2023-01-10 06:49:59 -0800

    finishing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+"""
+10-rectangle.py
+a class Rectangle that inherits from Rectangle (9-rectangle.py)
+add calculate area and size
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """calculate size and validate with integer validator"""
     def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
+        """validar si es un integer y retornar area"""
+        self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
+        """super sirve para modificar en la clase padre"""
+
+    def __str__(self):
+        """print rectangle width/height"""
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
 
